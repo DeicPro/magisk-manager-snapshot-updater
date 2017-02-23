77dfb79d7b37277755668f78498fcbd0f3157e29
Deiki-kun <DeicPro@users.noreply.github.com>
2017-02-23 19:32:19 +0100

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,7 +1,6 @@
 #!/system/bin/sh
 
 mod=${0%/*}
-version_file=magish_manager_version.sh
 update_version_file=magisk_manager_update.sh
 wget=$mod/wget
 bbx=/data/magisk/busybox
@@ -16,12 +15,12 @@ while :; do
 done
 
 update(){
-    $wget --no-check-certificate -O $tmp/$update_version_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/updates/$update_version_file
-    source $mod/$version_file
+    $wget --no-check-certificate -O $mod/$update_version_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/updates/$update_version_file
+
     source $tmp/$update_version_file
     if [ "$version" ] && [ "$lastest_version" ] && [ ! "$lastest_version" == "$version" ]; then
         $wget --no-check-certificate -O $tmp/$apkname $download_url
-        $wget --no-check-certificate -O $mod/$version_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/$version_file
+
         pm install -r $tmp/$apkname
         am start com.topjohnwu.magisk/.SplashActivity
     fi
