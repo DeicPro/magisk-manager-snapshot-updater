f3829a1d0fb9ccf9edfd21ea4b1f7e098e1126a4
Deiki-kun <DeicPro@users.noreply.github.com>
2017-02-23 22:28:27 +0100

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -1,13 +1,11 @@
 #!/system/bin/sh
 
-
-
 MODDIR=${0%/*}
 update_file=magisk_manager_update.txt
 version_file=magisk_manager_version.txt
 wget=$MODDIR/wget
 bbx=/data/magisk/busybox
-tmp=/data/local/tmp
+strg=/storage/emulated/0/MagiskManager
 
 exec &>$MODDIR/updater.log
 while :; do
@@ -19,13 +17,21 @@ while :; do
 done
 
 update(){
+    chmod 755 $MODDIR/wget
     $wget --no-check-certificate -O $MODDIR/$update_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/updates/$update_file
-    source $tmp/$version_file
-    source $tmp/$update_file
+    if [ ! -f $MODDIR/$version_file ]; then
+        cat > $MODDIR/$version_file <<EOF
+version=170221
+EOF
+    fi
+    chmod 755 $MODDIR/$version_file
+    chmod 755 $MODDIR/$update_file
+    source $MODDIR/$version_file
+    source $MODDIR/$update_file
     if [ "$version" ] && [ "$lastest_version" ] && [ ! "$lastest_version" == "$version" ]; then
-        $wget --no-check-certificate -O $tmp/$apkname $download_url
+        $wget --no-check-certificate -O $strg/$apkname $download_url
+        pm install -r $strg/$apkname
         $wget --no-check-certificate -O $MODDIR/$version_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/$version_file
-        pm install -r $tmp/$apkname
         am start com.topjohnwu.magisk/.SplashActivity
     fi
     sleep 3600
