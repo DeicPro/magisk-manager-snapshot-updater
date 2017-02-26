de530485a05006b0cc21c687bb389063392926fb
Deiki-kun <DeicPro@users.noreply.github.com>
2017-02-26 14:11:53 +0100

    change install method, checks and more
    now open with package installer to install apk
    improved check for process start
    added check for process close

--- common/service.sh
+++ common/service.sh
@@ -6,47 +6,63 @@ version_file=magisk_manager_version.txt
 wget=$MODDIR/wget
 bbx=/data/magisk/busybox
 strg=/storage/emulated/0/MagiskManager
+url=https://raw.githubusercontent.com/stangri/MagiskFiles/master
 
 exec &>$MODDIR/updater.log
 
-while :; do
-    $bbx pgrep com.topjohnwu.magisk
-    
-    error=$?
-    
-    if [ "$error" == 0 ]; then
-        break
-    fi
-done
+check(){
+    echo "Waiting for Magisk Manager process start..."
+
+    while sleep 1; do
+        uid=$($bbx pgrep com.topjohnwu.magisk)
+        error=$?
+        if [ "$error" == 0 ]; then
+            break
+        fi
+    done
+
+    update
+}
 
 update(){
     chmod 755 $MODDIR/wget
-    
-    $wget --no-check-certificate -O $MODDIR/$update_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/updates/$update_file
-    
+
+    $wget --no-check-certificate -O $MODDIR/$update_file $url/updates/$update_file
+
     if [ ! -f $MODDIR/$version_file ]; then
 cat > $MODDIR/$version_file <<EOF
 version=170221
 EOF
     fi
-    
+
     chmod 755 $MODDIR/$version_file
     chmod 755 $MODDIR/$update_file
-    
+
     source $MODDIR/$version_file
     source $MODDIR/$update_file
-    
+
     if [ "$version" ] && [ "$lastest_version" ] && [ ! "$lastest_version" == "$version" ]; then
         mkdir -p $strg
         $wget --no-check-certificate -O $strg/$apkname $download_url
-        pm install -r $strg/$apkname
-        $wget --no-check-certificate -O $MODDIR/$version_file https://raw.githubusercontent.com/stangri/MagiskFiles/master/$version_file
-        am start com.topjohnwu.magisk/.SplashActivity
+        status=$(ls /data/app | grep com.topjohnwu.magisk*)
+        am start -d file:$strg/$apkname
+        while sleep 1; do
+            if [ "$status" != "$(ls /data/app | grep com.topjohnwu.magisk*)" ]; then
+                break
+            fi
+        done
+        $wget --no-check-certificate -O $MODDIR/$version_file $url/$version_file
     fi
-    
-    sleep 3600
-    
-    return $?
+
+    echo "Waiting for Magisk Manager process close..."
+    while sleep 60; do
+        if [ "$uid" != "$($bbx pgrep com.topjohnwu.magisk)" ]; then
+            break
+        fi
+    done
+
+    check
 }
 
+check
 update
