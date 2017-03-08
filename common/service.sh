31680b94d9e7e81954408bbc83e60072b0c52500
DeicPro <DeicPro@users.noreply.github.com>
2017-03-08 03:47:14 +0100

    trying to fix install_tool bug

--- common/service.sh
+++ common/service.sh
@@ -10,7 +10,7 @@ cat /system/build.prop | grep ro.build
 cat /system/build.prop | grep ro.product
 cat /system/build.prop | grep ro.board
 
-module_version=3.0.1
+module_version=3.1.0
 module_update_file=module_update.txt
 update_file=magisk_manager_update.txt
 version_file=magisk_manager_version.txt
@@ -26,7 +26,7 @@ toast() { am start -a android.intent.action.MAIN -e message "Magisk Manager Snap
 $1" -n com.rja.utility/.ShowToast; }
 
 module_update() {
-    notification "Checking for module updates..."
+    #notification "Checking for module updates..."
 
     download "$MODDIR/$module_update_file $url/updates/$module_update_file"
 
@@ -35,17 +35,17 @@ module_update() {
     source $MODDIR/$module_update_file
 
     if [ "$module_version" ] && [ "$module_lastest_version" ] && [ "$module_lastest_version" != "$module_version" ]; then
-        notification "Downloading ${module_file}..."
-        toast "Downloading ${module_file}..."
+        notification "Downloading module v${module_lastest_version}..."
+        #toast "Downloading module v${module_lastest_version}..."
         download "$strg/$module_file $module_download_url"
         notification "Updating module..."
-        toast "Updating module..."
+        #toast "Updating module..."
         $bbx unzip -o $strg/$module_file module.prop common/service.sh common/wget -d $strg
         cp -f $strg/module.prop $MODDIR/module.prop
         cp -f $strg/common/service.sh $MODDIR/service.sh
         cp -f $strg/common/wget $MODDIR/wget
-        notification "Module updated"
-        toast "Module updated"
+        notification "Module successfully updated"
+        #toast "Module successfully updated"
         sh $MODDIR/service.sh &
         exit
     fi
@@ -54,7 +54,7 @@ module_update() {
 }
 
 update() {
-    notification "Checking for Magisk Manager updates..."
+    #notification "Checking for Magisk Manager updates..."
 
     download "$MODDIR/$update_file $url/updates/$update_file"
 
@@ -70,39 +70,39 @@ update() {
 
     if [ "$version" ] && [ "$lastest_version" ] && [ "$lastest_version" != "$version" ]; then
         [ ! -f $strg ] && { mkdir -p $strg; }
-        notification "Downloading ${apk_file}..."
-        toast "Downloading ${apk_file}..."
+        notification "Downloading Magisk Manager v${lastest_version}..."
+        #toast "Downloading Magisk Manager v${lastest_version}..."
         installing=0
         install_tool "com.topjohnwu.magisk" "$apk_file" "$download_url"
         if [ "$installing" == 1 ]; then
             echo "version=$lastest_version" > $MODDIR/$version_file
-            notification "Magisk Manager updated" "am start --user 0 -a android.intent.action.MAIN -n com.topjohnwu.magisk/.SplashActivity"
-            toast "Magisk Manager updated"
+            notification "Magisk Manager successfully updated" "am start --user 0 -a android.intent.action.MAIN -n com.topjohnwu.magisk/.SplashActivity"
+            #toast "Magisk Manager successfully updated"
         fi
     fi
 
-    notification "You are up-to-date" "am start --user 0 -a android.intent.action.MAIN -n com.topjohnwu.magisk/.SplashActivity"
+    #notification "You are up-to-date" "am start --user 0 -a android.intent.action.MAIN -n com.topjohnwu.magisk/.SplashActivity"
 
     #$bbx sed -i '/WARNING: cannot verify/d' $MODDIR/updater.log
     #$bbx sed -i '/Unable to locally verify/d' $MODDIR/updater.log
 
     #exec &>> $MODDIR/updater.log
 
-    sleep 7200
+    sleep 600
 
     module_update
 }
 
 install_tool() {
+    [ "$(pm list packages)" ] && { get_pkg=1; [ "$installing" ] && { pm uninstall -k $1; }; wait=0; } || { get_pkg=2; apk_number=$(ls /data/app | grep com.topjohnwu.magisk*); wait=10; }
+
     if [ -f $MODDIR/config.txt ]; then
         chmod 755 $MODDIR/config.txt
         source $MODDIR/config.txt
     fi
 
-    [ "$installing" ] && { pm uninstall -k $1; }
-
     while :; do
-        if [ ! "$(pm list packages | grep $1)" ]; then
+        if [ "$get_pkg" == 1 ] && [ ! "$(pm list packages | grep $1)" ] || [ "$get_pkg" == 2 ] && [ "$apk_number" == "$(ls /data/app | grep com.topjohnwu.magisk*)" ]; then
             if [ ! "$download" ]; then
                 download "$strg/$2 $3"
                 download=1
@@ -111,14 +111,14 @@ install_tool() {
                 if [ "$pm" != 0 ]; then
                     if [ "$installing" ]; then
                         notification "Updating Magisk Manager..."
-                        toast "Updating Magisk Manager..."
+                        #toast "Updating Magisk Manager..."
                     fi
                     pm install -r $strg/$2 &
                     install=1
                 else
                     if [ "$installing" ]; then
-                        notification "Tap to install ${apk_file}" "am start --user 0 -d file:$strg/$2"
-                        toast "Tap the notification to install ${apk_file}"
+                        notification "Tap to install new Magisk Manager v${lastest_version}" "am start --user 0 -d file:$strg/$2"
+                        #toast "Tap the notification to install new Magisk Manager v${lastest_version}"
                     else
                         am start -d file:$strg/$2
                     fi
@@ -132,7 +132,8 @@ install_tool() {
                 pm=0
             fi
         else
-            unset download install
+            sleep $wait
+            unset download install wait_install
             [ "$installing" ] && { installing=1; }
             break
         fi
