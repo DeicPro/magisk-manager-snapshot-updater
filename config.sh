4dab33a27326bb9f52138831af0d89343ff1ddb5
DeicPro <DeicPro@users.noreply.github.com>
2017-03-16 03:29:30 +0100

    update version

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ MODID=magisk-manager-snapshot-updater
 print_modname() {
     ui_print "*******************************"
     ui_print "  Magisk Manager Snapshot"
-    ui_print "  Updater Module v3.2.2"
+    ui_print "  Updater Module v3.2.3"
     ui_print "  By Deiki (@Deic/DeicPro)"
     ui_print "*******************************"
 }
